f9259ed8481a0f0ce06364eb16c9997d6d119df2
dphan <dphan@PHANI>
2023-08-03 20:42:35 +0530

    Iphone details uplord

---
+++ src/test/java/Practice/IPhone.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.junit.Test;
+
+public class IPhone {
+	
+	@Test
+	public void nothing()
+	{
+		System.out.println("hello world ");
+		
+		System.out.println("Iphone detiles");
+	}
+}
